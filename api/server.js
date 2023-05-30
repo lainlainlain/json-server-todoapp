3024d9910d72ec7aa032dd77c9b10f3a759c64eb
layerl <layerlain@yandex.ru>
2023-05-31 04:26:36 +0900

    cors

--- api/server.js
+++ api/server.js
@@ -1,5 +1,6 @@
 // See https://github.com/typicode/json-server#module
 const jsonServer = require('json-server');
+const cors = require('cors');
 const server = jsonServer.create();
 const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
@@ -14,6 +15,7 @@ server.use(
   }),
 );
 server.use(router);
+server.use(cors());
 server.listen(3000, () => {
   console.log('JSON Server is running');
 });
